88863a5d28a6dd24a6eac35c670dd286ab4e75ea
bo7owers <7orres.rene@proton.me>
2022-12-11 12:19:12 -0500

    Fix a11y

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="text-center">
-    <h2 class="text-green text-2xl">
+    <p class="text-green text-2xl">
       <span class="bg-gray-200 px-3.5 py-1 rounded text-sm font-bold">Advent of Vue</span>
-    </h2>
+    </p>
     <h1 class="text-4xl mt-2 mb-5"><slot></slot></h1>
   </header>
 </template>
