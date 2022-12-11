e5c82ad8409507f4894bb572ab1e0af42d5503cd
bo7owers <7orres.rene@proton.me>
2022-12-11 12:18:25 -0500

    Cleanup project

--- src/main.js
+++ src/main.js
@@ -11,20 +11,17 @@ import Button from 'primevue/button'
 import 'primevue/resources/primevue.min.css'
 import 'primevue/resources/themes/lara-light-purple/theme.css'
 import 'primeicons/primeicons.css'
-import { createPinia } from 'pinia'
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
 
-const pinia = createPinia()
 const app = createApp(App)
 
 app.use(PrimeVue)
 app.component('Dialog', Dialog)
 app.component('Button', Button)
 app.component('Card', Card)
-app.use(pinia)
 app.use(router)
 app.mount('#app')
