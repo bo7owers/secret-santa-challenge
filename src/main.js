8fa3ece7db98be809ab568f0d70d5a2452c7972c
bo7owers <7orres.rene@proton.me>
2022-12-10 21:13:34 -0500

    Add button comp form primevue

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { createRouter } from 'vue-router'
 import routes from '~pages'
 import Card from 'primevue/card'
 import Dialog from 'primevue/dialog'
+import Button from 'primevue/button'
 import 'primevue/resources/primevue.min.css'
 import 'primevue/resources/themes/lara-light-purple/theme.css'
 const router = createRouter({
@@ -14,4 +15,11 @@ const router = createRouter({
   routes,
 })
 
-createApp(App).use(PrimeVue).component('Dialog', Dialog).component('Card', Card).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue)
+app.component('Dialog', Dialog)
+app.component('Button', Button)
+app.component('Card', Card)
+app.use(router)
+app.mount('#app')
